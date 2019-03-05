07286ab6f6eb8947535932f6e15b8fd09751bc95
WangYuBo <646319863@qq.com>
2019-03-05 16:48:41 +0800

    创建resize文件

--- 01/2_resize.py
+++ 01/2_resize.py
@@ -1,12 +1,21 @@
-import  cv2
-import math
+import cv2
 
+# 高斯模糊值
+ksize = (15, 15)
+
+sigma = 0
 # 读取图片
-img = cv2.imread("./lena.jpg")
+img = cv2.imread("/media/wyb/work/github-local-resp/ai_learning/01/lena.jpg", 0)
+
+g_img = cv2.GaussianBlur(img, ksize, sigma)
 
 
+cv2.imshow('src_img', img)
 
-# 显示图片
-cv2.imshow('lena img ', img)
+cv2.imshow('g_img', g_img)
+
+res_img = cv2.resize(img, (1000, 1000))
+
+cv2.imshow('res_img', res_img)
 
 cv2.waitKey(0)
