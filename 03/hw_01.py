f5795160cca4cf12bc93f87e9ca5af04cbb04d0a
WangYuBo <646319863@qq.com>
2019-03-06 16:52:14 +0800

    第二周视觉计算图像滤波作业;问答第一题及答案图片;

--- 03/hw_01.py
+++ 03/hw_01.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # 卷积核大小
-ksize = (9, 9)
+ksize = (3, 3)
 
 # 读取原始图像
 src = cv2.imread('./lena.jpg')
@@ -13,19 +13,29 @@ b_img = cv2.blur(src, ksize)
 
 cv2.imshow('平均滤波', b_img)
 
+
+# 使用中值滤波处理图像
+
+m_img = cv2.medianBlur(src, 3, 0)
+
+cv2.imshow('中值滤波处理图', m_img)
+
+
 # 使用高斯平滑滤波处理图像
 g_img = cv2.GaussianBlur(src, ksize, sigmaX=0, sigmaY=0)
 
 cv2.imshow('高斯滤波处理的图像', g_img)
 
 
-# 使用中值滤波处理图像
+# 使用图像图形学处理
+a_img = cv2.morphologyEx(src, cv2.MORPH_OPEN, ksize, 1)
+b_img = cv2.morphologyEx(src, cv2.MORPH_CLOSE, ksize, 1)
 
-m_img = cv2.medianBlur(src, 15, 0)
-
-cv2.imshow('中值滤波处理图', m_img)
-
-
-
+cv2.imshow('图形形态学', b_img)
 
 cv2.waitKey(0)
+
+"""
+作业总结:
+卷积核为(3,3),中值为3时,观察lena睫毛及眼睛,图形形态学先开后闭,图形图像学最清晰,中值比高斯清晰,均值最模糊.
+"""
